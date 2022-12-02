89244759f46113a78dd0d930665ed9db7fd7176f
Bilguun <bg13083002@gmail.com>
2022-12-02 13:51:42 +0800

    Initial Commit

---
+++ Just./Just_App.swift
@@ -0,0 +1,17 @@
+//
+//  Just_App.swift
+//  Just.
+//
+//  Created by Bilguun Enkhbayar on 2022.12.02.
+//
+
+import SwiftUI
+
+@main
+struct Just_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
